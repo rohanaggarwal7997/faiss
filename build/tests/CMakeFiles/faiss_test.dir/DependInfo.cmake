ae5b4645be89bc68f8a6671c6a3917906cd25931
Rohan Aggarwal <rohaagga@rohaagga-mac.attlocal.net>
2023-08-29 13:33:40 -0700

    rohan changes

---
+++ build/tests/CMakeFiles/faiss_test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/rohaagga/Desktop/Faiss/faiss/tests/test_binary_flat.cpp" "tests/CMakeFiles/faiss_test.dir/test_binary_flat.cpp.o" "gcc" "tests/CMakeFiles/faiss_test.dir/test_binary_flat.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/rohaagga/Desktop/Faiss/faiss/build/faiss/CMakeFiles/faiss.dir/DependInfo.cmake"
+  "/Users/rohaagga/Desktop/Faiss/faiss/build/_deps/googletest-build/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/Users/rohaagga/Desktop/Faiss/faiss/build/_deps/googletest-build/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/rohaagga/Desktop/Faiss/faiss/tests
+# Build directory: /Users/rohaagga/Desktop/Faiss/faiss/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/Users/rohaagga/Desktop/Faiss/faiss/build/tests/faiss_test[1]_include.cmake")
+subdirs("../_deps/googletest-build")
